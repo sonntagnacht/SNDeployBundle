5e1b20464703f519e4639ace4793082f42d5dda0
Thomas Hanke <thomas@it-hanke.de>
2017-02-03 09:31:57 +0100

    add version service

--- Services/Version.php
+++ Services/Version.php
@@ -23,15 +23,34 @@ class Version
         $this->root     = $root;
     }
 
+    /**
+     * @return string
+     */
     public function getVersion()
     {
-        $deploy_file = "%s/../deploy.json";
+        $deploy_file = sprintf("%s/../deploy.json", $this->root);
         if(file_exists($deploy_file)) {
             $json = file_get_contents(sprintf($deploy_file, $this->root));
             $json = json_decode($json, true);
+            return $json["version"];
         }
 
         return "__DEV__";
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getCommit(){
+
+        $deploy_file = sprintf("%s/../deploy.json", $this->root);
+        if(file_exists($deploy_file)) {
+            $json = file_get_contents(sprintf($deploy_file, $this->root));
+            $json = json_decode($json, true);
+            return $json["commit"];
+        }
+
+        return null;
     }
 
 }
